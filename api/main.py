3fd741f2051324bf64889d7601cf46b0cd471bac
unknown <caio.bndantas@gmail.com>
2025-09-29 18:41:46 -0300

    Atualizando os endpoints e criando os endpoints opcionais

--- api/main.py
+++ api/main.py
@@ -32,7 +32,7 @@ def retornar_livros():
     df_books = df.to_dict(orient='records')
     return df_books
 
-#Esta função deve ficar acima da get id , ocorre um erro envolvendo FastAPI no caso contrário 
+
 @app.get('/api/v1/books/search',response_model=list[Books])
 def pesquisar_livros_titulo_categoria(title: str = None ,category:str = None):
     df_book = df.copy()
@@ -43,18 +43,13 @@ def pesquisar_livros_titulo_categoria(title: str = None ,category:str = None):
         df_book  = df_book[df_book['category'] == category]
     
     return df_book.to_dict(orient='records')
-
-@app.get('/api/v1/books/{id}',response_model=Books)
-def pesquisar_livros_id(id:int):
-    coluna   = df.iloc[id]
-    df_books = coluna.to_dict()
+    
+    
+@app.get('/api/v1/categories')
+def retornar_categorias():
+    df_books = list(set(df['category']))
     return df_books
     
-
-@app.get('/api/v1/categories',response_model=Books)
-def retornar_categories():
-    df_books = set(df['category'])
-    return df_books
     
 @app.get('/api/v1/health')
 def retorna_saude():
@@ -68,6 +63,29 @@ def retorna_saude():
             detail='Serviço indisponível: O Dataframe com os livros não foram carregados corretamente.'
         )
         
+#Endpoints opcionais 
         
-            
-       
+@app.get('/api/v1/stats/categories') 
+def estatica_detalhada():
+    return df.describe().to_dict()
+        
+        
+@app.get('/api/v1/books/top-rated')
+def livros_mais_avaliado():
+    df_book = df[['title']].copy()
+    df_book['reviews'] = 0
+    return df_book.to_dict(orient='records')
+
+@app.get('/api/v1/books/price-range',response_model=list[Books])
+def filtrar_preco(min:int,max:int):
+    filtro = df['price_incl_tax'].between(min,max)
+    df_book=df[filtro]
+    return df_book.to_dict(orient='records')
+        
+
+#Esta é uma função dinamica e de ficar abaixo das que não são ,se não dá erro 
+@app.get('/api/v1/books/{id}',response_model=Books)
+def pesquisar_livros_id(id:int):
+    coluna   = df.iloc[id]
+    df_books = coluna.to_dict()
+    return df_books
