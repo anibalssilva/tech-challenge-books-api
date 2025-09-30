b169ab2b9292c8a6be5b1e15fb4bcc1afc2e5b55
unknown <caio.bndantas@gmail.com>
2025-09-29 21:09:30 -0300

    Conclusão do básico dos endpoints obrigatórios e opcionais

--- api/main.py
+++ api/main.py
@@ -23,15 +23,16 @@ app = FastAPI()
 #Salvado o caminho para ser utilizado na criação do Dataframe
 pasta_dados  = Path(__file__).parent.parent
 caminho_dados= pasta_dados/'data'/'processed'/'books.csv'
+#Dataframe que será utilizado na api
 df =pd.read_csv(caminho_dados)
 
-
+#Retorna todos os livros com seus dados
 @app.get('/api/v1/books',response_model=list[Books])
 def retornar_livros():
     df_books = df.to_dict(orient='records')
     return df_books
 
-
+#Busca o livro pelo nome , pode buscar livros pela categoria também e pode buscar um livro por ambos
 @app.get('/api/v1/books/search',response_model=list[Books])
 def pesquisar_livros_titulo_categoria(title: str = None ,category:str = None):
     df_book = df.copy()
@@ -43,13 +44,13 @@ def pesquisar_livros_titulo_categoria(title: str = None ,category:str = None):
     
     return df_book.to_dict(orient='records')
     
-    
+#Retorna uma lista com todas as categoria sem duplicatas 
 @app.get('/api/v1/categories')
 def retornar_categorias():
     df_books = list(set(df['category']))
     return df_books
     
-    
+#Verifica se o Dataframe com os dados dos livros foi carregado com sucesso, retornando a quantidade de livros  
 @app.get('/api/v1/health')
 def retorna_saude():
     if df is not None and not df.empty:
@@ -63,12 +64,28 @@ def retorna_saude():
         )
         
 #Endpoints opcionais 
-        
+
+
+@app.get('/api/v1/stats/overview')
+def estatistica_geral():
+    dict_estatistica_geral  = {'total_de_livros': len(df['title']),'preco_medio': round(df['price_incl_tax'].mean(),2)
+                              , 'maior_preco': df['price_incl_tax'].max(),'menor_preco':df['price_incl_tax'].min(),}
+    dict_estatistica_ratings= {'avaliacao_media':df['rating'].mean(),'distribuicao_de_avaliacoes':df['rating'].value_counts().to_dict() }
+    
+    return dict_estatistica_geral,dict_estatistica_ratings
+
+
+#Gera estatísticas detalhadas por categoria 
 @app.get('/api/v1/stats/categories') 
 def estatica_detalhada():
-    return df.describe().to_dict()
+    dict_estatistica_categorias = {'total_de_categorias':df['category'].nunique() ,
+                                   'quantidade_por_categoria':df['category'].value_counts().round(2).to_dict(),
+                                   'preco_medio_categoria':df.groupby('category')['price_incl_tax'].mean().round(2).to_dict(),
+                                   'avaliacao_por_categoria': df.groupby('category')['rating'].mean().round(2).to_dict()
+                                   }
+    return dict_estatistica_categorias    
         
-        
+#Retorna exatamento o nome e avaliação de todos livros por ordem da maior avaliação        
 @app.get('/api/v1/books/top-rated')
 def livros_mais_avaliado():
     df_book = df[['title']].copy()
@@ -76,6 +93,7 @@ def livros_mais_avaliado():
     df_book= df_book.sort_values(by='rating',ascending=False)
     return df_book.to_dict(orient='records')
 
+#Retorna os livros através de um preço mínimo e máximo respectivamente
 @app.get('/api/v1/books/price-range',response_model=list[Books])
 def filtrar_preco(min:int,max:int):
     filtro = df['price_incl_tax'].between(min,max)
@@ -84,6 +102,7 @@ def filtrar_preco(min:int,max:int):
         
 
 #Esta é uma função dinamica e de ficar abaixo das que não são ,se não dá erro 
+#Retorna o livro pelo seu id no Dataframe ,o id do Dataframe começa com 0
 @app.get('/api/v1/books/{id}',response_model=Books)
 def pesquisar_livros_id(id:int):
     coluna   = df.iloc[id]
